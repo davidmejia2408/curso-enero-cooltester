59116a16ac9517a296d7557abd3c343450ffc183
David MPC <David MPC@DESKTOP-QTJC8A9>
2022-02-08 20:51:05 -0600

    a

--- SeleniumCursoEnero/src/test/java/com/java/DataTypes.java
+++ SeleniumCursoEnero/src/test/java/com/java/DataTypes.java
@@ -1,119 +1,68 @@
 package com.java;
 
+import java.util.Scanner;
+
 public class DataTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		// Datos primitivos
+		Scanner ref = new Scanner(System.in);
+		
+		do{
+			
+		System.out.println("Por favor ingresa el ejerciio a visualizar");
+		int ejercicio = ref.nextInt();
+		
 
-		byte var1 = 1; // Numerico entero
-		short var2 = 1; // Numerico entero
-		int var3 = 1; // Numerico entero (Entero) - Automation
-		long var4 = 1L; // Numerico entero - Automation
-		float var5 = 1.2f; // Numerico real
-		double var6 = 2.223; // Numerico real (decimales) - Automation
-		char var7 = 'a'; // Caracter - Automation
-		boolean var8 = true; // Boleano - Automation - tiene dos valores TRUE / FALSE
+		switch (ejercicio) {
+		case 1:
+			
+			System.out.println("Usted ha seleccionado el ejercicio número 1");
 
-		// Declarar variables
-		// int x;
-//		x = 10000;
-		//sdasdas
+			break;
 
-		// int y = 10;
+		case 2:
+			
+			System.out.println("Usted ha seleccionado el ejercicio número 2");
 
-		// Char symbol example
-		char symbol = '\u00A9';
+			break;
+		case 3:
+			
+			System.out.println("Usted ha seleccionado el ejercicio número 3");
 
-		// Cadena Char
-		String message = "Hello world"; // - Automation
+			break;
+		case 4:
+			
+			System.out.println("Usted ha seleccionado el ejercicio número 4");
 
-		// Imprimir en consola
-		System.out.println(symbol);
-		System.out.println(message);
+			break;
+		case 5:
+			
+			System.out.println("Usted ha seleccionado el ejercicio número 5");
 
-		// Concatenacion, Se representa con el signo +
-		System.out.println(message + symbol);
+			break;
+		case 6:
+			
+			System.out.println("Usted ha seleccionado el ejercicio número 6");
 
-		int numero = 1;
-		String nombre = "Ricardo";
-		System.out.println(nombre + numero);
+			break;
+		case 7:
+			
+			System.out.println("Usted ha seleccionado el ejercicio número 7");
 
-		// OPERATORS
+			break;
 
-		// Operadores aritmeticos
-		int x = 2;
-		int y = 2;
-
-		int suma = x + y;
-		int resta = x - y;
-		int mult = x * y;
-		int div = x / y;
-
-		double z = 2.23;
-		double a = 4.34;
-
-		double mult2 = z * a;
-
-		System.out.println("Esta es la suma " + suma);
-		System.out.println("Esta es la resta " + resta);
-		System.out.println("Esta es la mult " + mult);
-		System.out.println("Esta es la div " + div);
-
-		System.out.println("El valor double es " + mult2);
-
-		// Operadores Unarios
-		int unario = 2;
-		++unario;
-		System.out.println(unario);
-
-		// Operadores relacionales
-//				Igual que ==
-//				Diferente a !=
-//				Mayor que >
-//				Mayor o igual que >=
-//				Menos que <
-//				Menor o igual que <=
-
-		int or = 2;
-		int or2 = 3;
-
-		if (or != or2) {
-			System.out.println("TRUE");
-		} else {
-			System.out.println("False");
+		default:
+			
+			System.out.println("Ese número de ejercicio no se encuentra en la lista"+ "\n" +"Por favor intente de acuerdo al menú mostrado");
+			
+			break;
 		}
-
-		String msg = "Hello";
-		String msg2 = "Hello";
-
-		if (msg != msg2) {
-			System.out.println("String TRUE");
-		} else {
-			System.out.println("String False");
-		}
-
-		// Operadores condicionales
-//				 AND &&
-//				 OR ||
-
-		int oper1 = 2;
-		int oper2 = 2;
-		int oper3 = 4;
-		int oper4 = 3;
-
-		if (oper1 == oper2 && oper3 == oper4 && msg == msg2) {
-			System.out.println("TODOS LOS VALORES SON IGUALES");
-		} else {
-			System.out.println("ALGUN VALOR ES DIFERENTE");
-		}
-
-		if (oper1 == oper2 || oper3 == oper4) {
-			System.out.println("TODOS LOS VALORES SON IGUALES");
-		} else {
-			System.out.println("ALGUN VALOR ES DIFERENTE");
-		}
+		System.out.println("¿Desea volver a elegir un ejercicio?"+ "\n" + "1. Si"+ "\n" +"2. No"+ "\n" +"Digita el número de la opción deseada");
+		int repetir = ref.nextInt();
+		
+		}while (repetir<2);			
+			
 
 	}
 }
